424e8d975298641c4049177a89a48cf794516bff
Susan Lunn <slgems@gmail.com>
2014-05-01 22:23:43 -0400

    changed so it doesn't use lists

--- FrameScrapper/spiders/glasses_spider.py
+++ FrameScrapper/spiders/glasses_spider.py
@@ -1,5 +1,7 @@
 from scrapy.spider import Spider
-from scrapy.selector import Selector
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.loader import XPathItemLoader
+from scrapy.contrib.loader.processor import Join, MapCompose
 
 from FrameScrapper.items import FramescrapperItem
 
@@ -8,13 +10,20 @@ class GlassesSpider(Spider):
     allowed_domains=["http://www.glasses.com/"]
     start_urls=["http://www.glasses.com/men-glasses"]
 
-    def parse(self, response):
-        sel = Selector(response)
-        sites = sel.xpath('//div[@class="content slider"]/div[1]/a')
-        items = []
+    urls_list_xpath = '//div[@class="content slider"]/div[1]/a'
+    item_fields = {'url': './@href'
+    }
 
-        for site in sites:
-            item = FramescrapperItem()
-            item['url'] = site.xpath('./@href').extract()
-            items.append(item)
-        return items
+    def parse(self, response):
+        sel = HtmlXPathSelector(response)
+
+        for site in sel.select(self.urls_list_xpath):
+            loader = XPathItemLoader(FramescrapperItem(), selector=site)
+
+            #define processes
+            loader.dafault_input_processor = MapCompose(unicode.strip)
+            loader.default_output_processor = Join()
+
+            for field, xpath in self.item_fields.iteritems():
+                loader.add_xpath(field, xpath)
+            yield loader.load_item()
