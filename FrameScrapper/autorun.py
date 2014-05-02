2ad22aafd5346f7854c15b6fc06c215d1a8a3849
Susan Lunn <slgems@gmail.com>
2014-05-02 07:02:21 -0400

    bash scripts

--- FrameScrapper/autorun.py
+++ FrameScrapper/autorun.py
@@ -2,20 +2,20 @@ import subprocess
 
 class AutoRunURLs():
 
-    """
-    subprocess.call("/.refreshjson", shell=True)
+    subprocess.call("/bashscripts/.refreshjson", shell=True)
     print("deleted items.json")
-    subprocess.call("/.coastal.sh", shell=True)
+    """
+    subprocess.call("/bashscripts/.coastal.sh", shell=True)
     print("coastal run")
-    subprocess.call("/.eyefly.sh", shell=True)
+    subprocess.call("/bashscripts/.eyefly.sh", shell=True)
     print("eyefly run")
-    subprocess.call("/.memmzer.sh", shell=True)
+    subprocess.call("/bashscripts/.memmzer.sh", shell=True)
     print("memmzer run")
-    subprocess.call("/.glasses.sh", shell=True)
+    subprocess.call("/bashscripts/.glasses.sh", shell=True)
     print("glasses run")
-    subprocess.call("/.thirtynine.sh", shell=True)
+    subprocess.call("/bashscripts/.thirtynine.sh", shell=True)
     print("thirtynine run")
-    subprocess.call("/.warbyparker.sh", shell=True)
+    subprocess.call("/bashscripts/.warbyparker.sh", shell=True)
     print("warbyparker run")
     """
     spiders = ["coastal", "eyefly", "mezzmer", "glasses", "thirtynine", "warbyparker"]
